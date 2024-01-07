f7daf00444c195c2df09098763265ba53f65e686
Jonathan Marrero <Jonathanmarrero24@gmail.com>
2024-01-06 19:04:03 -0500

    live portfolio

---
+++ src/constants/index.js
@@ -0,0 +1,283 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+    edelstein,
+    general,
+    moist,
+    zoo,
+    bookmark,
+    tracker,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "React Native Developer",
+      icon: mobile,
+    },
+    {
+      title: "Front-End Developer",
+      icon: backend,
+    },
+    {
+      title: "Content Creator",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    {
+      name: "docker",
+      icon: docker,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Operational Specialist",
+      company_name: "Edelstein & Comapny LLP",
+      icon: edelstein,
+      iconBg: "#E6DEDD",
+      date: "January 2023 - Present",
+      points: [
+        "Run all technology operations ensuring all proper e-files and technology is up to date and running with full functionality.",
+        "Manage technology issues with servers, software systems, and company applications.",
+        "Increased server production, response time, and speed by upgrading the RAM to improve the performance of the company server.",
+        "Analyze various server reports to maximize functionality of all company servers.",
+      ],
+    },
+    {
+      title: "Lead Software Engineer",
+      company_name: "Moist Cookie Co.",
+      icon: moist,
+      iconBg: "#383E56",
+      date: "January 2022 - Present",
+      points: [
+        "Oversee entire software development lifescycle from design to testing.",
+        "Apply principles of software engineering to design, build, and maintain machines and structures.",
+        "Increase site engagement by 55% by building a better UX/UI experience for customers.",
+        "Perform developer testing, fix bugs, and prepare and install solutions.",
+        "Collaborate with stakeholders on changes, design, and functionality of company site."
+      ],
+    },
+    {
+      title: "Software Engineer Fellow",
+      company_name: "General Assembly",
+      icon: general,
+      iconBg: "#383E56",
+      date: "September 2022 - April 2023",
+      points: [
+        "Mastered 10+ frameworks and stacks, applying essential object-oriented programming concepts, leveraging JavaScript libraries, and incorporating functionality from third-party API's to create rich and robust web applications.",
+        "Completed 500+ hours of Full-Stack Software Engineering, applying expert-led training and the industry's most in-demand technologies on a variety of software development projects.",
+        "Collaborated with 5+ cross-functional teams using Agile Development workflow to carry out mission-driven projects from concept to completion, utilizing real-time collaboration tools including Git and GitHub.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "After Jonathan optimized our website, our traffic increased by 55%. He is a great asset to the team!",
+      name: "Alex Kyprianou",
+      designation: "CEO",
+      company: "Moist Cookie Co.",
+      image: "https://media.licdn.com/dms/image/C5603AQFyJUAh59Dm1w/profile-displayphoto-shrink_800_800/0/1528725753827?e=1709769600&v=beta&t=nmRgR4nrX5d0-0wQ6IZKdpJpBiBA3ZTnqLZQYaQWYi0",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their own personal growth like Jonathan does.",
+      name: "Baron Osei-Bonsu",
+      designation: "CSM",
+      company: "Decile",
+      image: "https://media.licdn.com/dms/image/D4E03AQGlnd7EHhARDA/profile-displayphoto-shrink_800_800/0/1681226376786?e=1709769600&v=beta&t=gcpYNG3C8osgFEMXqO4ozMMGR5mZNhRADfHIifqptt8",
+    },
+    {
+      testimonial:
+        "Jonathan helped me grow my website and made it easier for me to keep track of my clients, I can't thank him enough!",
+      name: "Dr. Joseph Heneine ",
+      designation: "Doctor",
+      company: "Physical Therapy",
+      image: "https://media.licdn.com/dms/image/C4E03AQGFReKH5vT7sA/profile-displayphoto-shrink_800_800/0/1582652499083?e=1709769600&v=beta&t=gviJ-iMMFexPBjtPnGDJD5_q-K-T3HmzvJ4CgxtqEKA",
+    },
+    {
+      testimonial:
+        "Jonathan's patience and support on my website allowed for him to create exactly what I was looking for, thanks Jonathan!",
+      name: "Joshua Lucas ",
+      designation: "Athletic Trainer",
+      company: "Pembroke HS",
+      image: "https://media.licdn.com/dms/image/C5603AQFIyAHwrrHZjQ/profile-displayphoto-shrink_800_800/0/1656420773976?e=1709769600&v=beta&t=0R4ZtoR_7rXz9Hko61M-hMM8rM0hOqovVfXBZJ9eWck",
+    },
+    {
+      testimonial:
+        "Jonathan's ideas and curiosity made being on his team a great experience for me as an engineer. ",
+      name: "Christian Hornedo",
+      designation: "Software Engineer",
+      company: "Beast Code",
+      image: "https://media.licdn.com/dms/image/C5603AQFT4AQVurQWog/profile-displayphoto-shrink_800_800/0/1616628633992?e=1709769600&v=beta&t=6sfmAtzBMacY90ekVwvNw9UohxznaJVtdSeHdGyoQDs",
+    },
+    {
+      testimonial:
+        "Thank you Jonathan, for helping us create new client sections on our website, we love it! ",
+      name: "Iris Plaza",
+      designation: "HR Manager",
+      company: "Prince Lobel Tye LLP",
+      image: "https://media.licdn.com/dms/image/C4D03AQG6e4FiNavwmQ/profile-displayphoto-shrink_800_800/0/1630514722139?e=1709769600&v=beta&t=G2tsTC-MBMDxt_mR-OdvxAoxCVKQOW_boROJxkWVoHU",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "JobOps",
+      description:
+        "Web-based platform that allows users to have full CRUD accesibility, manage jobs from various companies, providing a convenient and efficient solution for job search needs.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "JavaScript",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Express",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tracker,
+      source_code_link: "https://github.com/jonmarrero/application-tracker-frontend",
+    },
+    {
+      name: "Bookmark'd",
+      description:
+        "Web application that enables users to track, create, update, and delete all personal bookmarks from start to finish, allowing the user to have full access to track their personal progress.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "JavaScript",
+          color: "green-text-gradient",
+        },
+        {
+          name: "BootStrap",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: bookmark,
+      source_code_link: "https://github.com/jonmarrero/frontend-bookmark",
+    },
+    {
+      name: "The Zoo",
+      description:
+        "A comprehensive fitness application that allows users to search workouts, track workouts, build workouts, and have full detailed examples of advanced strength and conditioning movements.",
+      tags: [
+        {
+          name: "React",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Python",
+          color: "green-text-gradient",
+        },
+        {
+          name: "SCSS",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: zoo,
+      source_code_link: "https://github.com/jonmarrero/finalcapstoneFE",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
